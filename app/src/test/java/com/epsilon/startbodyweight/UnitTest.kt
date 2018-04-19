1f5187ea586c01298433bd673cde947dda4ed489
CAE.CA\tdjeilati <thaer.jleilati@cae.com>
2018-04-19 16:24:42 -0400

    Added basic unit test file

---
+++ app/src/test/java/com/epsilon/startbodyweight/UnitTest.kt
@@ -0,0 +1,20 @@
+package com.example.android.startbodyweight
+
+
+import org.junit.Test
+
+import org.junit.Assert.assertEquals
+
+/**
+ * Local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class StartBodyWeightUnitTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun inner_class_exists() {
+        assertEquals("There should be 1 Inner class inside the contract class", 1, 6)
+    }
+}
