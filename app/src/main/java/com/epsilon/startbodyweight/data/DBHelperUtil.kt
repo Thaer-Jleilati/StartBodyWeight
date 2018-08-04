b8fed580982ce87f0410f145717801ea31b08187
Thaer Jleilati <thaer.jle@gmail.com>
2018-08-04 04:25:42 -0400

    Removed useless func
    Signed-off-by: Thaer Jleilati <thaer.jle@gmail.com>

--- app/src/main/java/com/epsilon/startbodyweight/data/DBHelperUtil.kt
+++ app/src/main/java/com/epsilon/startbodyweight/data/DBHelperUtil.kt
@@ -1,15 +1,7 @@
 package com.epsilon.startbodyweight.data
 
-import android.content.res.Resources
-
 class DBHelperUtil {
     companion object {
-        fun populateDbTestData(db: RoomDB?, resources: Resources) {
-            val exers = ExerData.getExerciseList(resources)
-            exers?.
-                    mapIndexed { i, it -> ExerciseEntity( it.name, i, it.progs[0], 0, 5, 6, 2, 0, false, 0) }?.
-                    forEach { db?.Dao()?.updateExercise(it) }
-        }
 
         fun nukeDatabase(db: RoomDB?) {
             db?.Dao()?.nukeTable()
